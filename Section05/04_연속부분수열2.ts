803078ee7673854b3307e245667a2022c920af30
Chris <dnchks08@gmail.com>
2023-01-22 17:33:01 +0900

    Daily: 01/22 코테

--- Section05/04_연속부분수열2.ts
+++ Section05/04_연속부분수열2.ts
@@ -1,14 +1,31 @@
 function solution5_4(num: number, arr: number[]): number {
   let count = 0;
-
   for (let i = 0; i < arr.length; i++) {
-    let answer = 0;
+    let sum = 0;
     for (let j = i; j < arr.length; j++) {
-      console.log(j);
+      sum += arr[j];
+
+      if (sum <= num) {
+        count++;
+      } else {
+        break;
+      }
     }
   }
-
-  return 3;
+  return count;
 }
 
 console.log(solution5_4(5, [1, 3, 1, 2, 3]));
+// let count = 0;
+
+// function hey(arr: number[], selectNum: number) {
+//   const result: number[][] = [];
+//   if (selectNum === 1) return arr.map((item) => [item]);
+
+//   arr.forEach((value, idx, array) => {
+//     const restArr = array.slice(idx + 1);
+//     const combine = hey(restArr, selectNum - 1);
+//     const attached = combine?.map((item) => [value, ...item]);
+//     result.push(...attached);
+//   });
+// }
--- Section05/05_최대매출.ts
+++ Section05/05_최대매출.ts
@@ -1,5 +1,27 @@
 function solution5_5(num: number, arr: number[]): number {
-  return 3;
+  let max = 0;
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (max < arr[i] + arr[i + 1] + arr[i + 2]) {
+      max = arr[i] + arr[i + 1] + arr[i + 2];
+    }
+  }
+  return max;
 }
 
 console.log(solution5_5(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]));
+
+// function solution5_5(num: number, arr: number[]): number {
+//   let max = 0;
+//   const resultArr: Array<number[]> = [];
+//   for (let i = 0; i < arr.length - 2; i++) {
+//     if (max < arr[i] + arr[i + 1] + arr[i + 2]) {
+//       max = arr[i] + arr[i + 1] + arr[i + 2];
+//       resultArr.pop();
+//       resultArr.push([arr[i], arr[i + 1], arr[i + 2]]);
+//     }
+//   }
+//   console.log("연속된 최고 매출", resultArr[0]);
+//   return max;
+// }
+
+// console.log(solution5_5(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]));
