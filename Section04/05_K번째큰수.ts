42fffacc91ed20ade31bee9310485f294e8aa2fe
Chris <dnchks08@gmail.com>
2023-03-13 18:57:25 +0900

    test: 로직 테스트 및 수정

--- Section04/05_K번째큰수.ts
+++ Section04/05_K번째큰수.ts
@@ -1,14 +1,29 @@
+// function solution4_5(len: number, num: number, arr: number[]): number {
+//   let tripleSum: number[] = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       for (let k = j + 1; k < arr.length; k++) {
+//         tripleSum.push(arr[i] + arr[j] + arr[k]);
+//       }
+//     }
+//   }
+//   return [...new Set(tripleSum)].sort((a, b) => b - a)[num - 1];
+// }
+
+// console.log(solution4_5(10, 3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]));
+
 function solution4_5(len: number, num: number, arr: number[]): number {
-  let tripleSum: number[] = [];
+  let tripleSum: Set<number> = new Set();
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       for (let k = j + 1; k < arr.length; k++) {
-        tripleSum.push(arr[i] + arr[j] + arr[k]);
+        tripleSum.add(arr[i] + arr[j] + arr[k]);
       }
     }
   }
-  return [...new Set(tripleSum)].sort((a, b) => b - a)[num - 1];
+  return [...tripleSum].sort((a, b) => b - a)[num - 1];
 }
 
 console.log(solution4_5(10, 3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]));
--- Section06/06_공주구하기.ts
+++ Section06/06_공주구하기.ts
@@ -1,11 +1,11 @@
 function solution6_6(num1: number, num2: number): number {
   let arr: number[] = Array.from({ length: num1 }, (_, i) => i + 1);
-  const stack: number[] = [];
+  const queue: number[] = [];
   while (true) {
     for (let i = 1; i < num2; i++) {
       const tmp = arr.shift();
-      if (tmp !== undefined) stack.push(tmp);
-      if (arr.length === 0) arr = stack;
+      if (tmp !== undefined) queue.push(tmp);
+      if (arr.length === 0) arr = queue;
     }
     arr.shift();
     if (arr.length === 1) return arr[0];
@@ -15,4 +15,4 @@ function solution6_6(num1: number, num2: number): number {
 console.log(solution6_6(8, 3));
 
 // arr = 7 8
-// stack = 2 4 7 8
+// queue = 2 4 7 8
